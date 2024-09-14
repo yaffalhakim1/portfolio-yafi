8761d3d950d9328a5760cebd2e4aad585c542af5
Muhammad Yafi Alhakim <yafialhakim64@gmail.com>
2024-09-14 10:05:19 +0700

    fix: pnpm problem and change work section

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -4,107 +4,36 @@ export default function ExperiencePage() {
   return (
     <>
       <h1 className="font-bold text-[36px]">Experience</h1>
-      <p className="mt-3">Here are some of my experience.</p>
-      <div className="flex-col">
-        <h3 className="mt-4 text-lg font-semibold ">
-          Frontend Engineer Trainee -{" "}
-          <span className="font-normal text-md">Shopee International</span>
-        </h3>
-        <p className="mt-2 mb-1 text-sm font-normal leading-none">
-          October 2023 - February 2024
-        </p>
-        <p className="mb-4 text-base font-normal dark:leading-relaxed">
-          This is an internship program from Shopee International. The Labs
-          Bootcamp consists of a four-month long program covering the software
-          engineering fundamentals, best coding practices on common tech stacks
-          and tools used in the enterprise development center, and near-the-job
-          training through simulated projects by the end of the program.
-          <ul className="mt-4 space-y-1 text-justify list-disc list-inside">
-            <li>
-              Achieved excellence in mastering programming essentials,
-              showcasing proficiency in Go, Javascript, and Typescript.
-            </li>
-            <li>
-              Completing all programming assignments in Go and Typescript,
-              consistently garnering positive accolades from mentors for
-              exemplary performance
-            </li>
-            <li>
-              Acquired expertise in web platform development, industry-leading
-              tech stacks such as React and NextJS.
-            </li>
-            <li>
-              Finished projects that requires NextJS (Fullstack apps), ReactJS,
-              and Typescript with positive feedbacks from seniors. Ex: Medium
-              Lite, HealthCare Platform
-            </li>
-          </ul>
-        </p>
-        <div className="my-4 border" />
-        <h3 className="mt-4 text-lg font-semibold ">
-          Front End Developer -{" "}
-          <span className="font-normal text-md">Universitas Diponegoro</span>
-        </h3>
-        <p className="mt-2 mb-1 text-sm font-normal leading-none">
-          December 2022 - April 2023
-        </p>
-        <p className="mb-4 text-base font-normal leading-relaxed ">
-          In this project, I work with 1 backend engineer and 1 hardware
-          engineer to create an internal web app for Diponegoro University
-          (Computer Engineering Department). The web app function is to manage
-          documents accreditations.
-          <ul className="mt-4 space-y-1 list-disc list-inside ">
-            <li>
-              Integrated client-side interactions with server-side processes
-              through APIs and Firebase.
-            </li>
-            <li>
-              Designed, implemented and delivered complex UI experiences using
-              NextJS with Typescript
-            </li>
-            <li>
-              Coordinated with engineering teams to develop and maintain
-              codebase
-            </li>
-            <li>
-              Created unit tests for ensuring the performance of front-end code
-            </li>
-          </ul>
-        </p>
-        <div className="my-4 border" />
 
-        <h3 className="mt-4 text-lg font-semibold ">
-          Mobile Developer Intern -{" "}
-          <span className="font-normal text-md">PT Errai Pasifik</span>
-        </h3>
-        <time className="mt-2 mb-1 text-sm font-normal leading-none">
-          July 2022 - October 2023
-        </time>
-        <p className="mb-4 text-base font-normal ">
-          In this project, I work with 1 Backend Engineer, 2 UI/UX Designer, and
-          1 Project Manager to create an app using Flutter. The app function is
-          to find tailor nearby.
-          <ul className="mt-4 space-y-1 leading-relaxed list-disc list-inside">
-            <li>
-              Designing UI (mobile app) for some screen and discussed it with
-              Product Manager
-            </li>
-            <li>
-              Slicing from design to a reusable, clean code and build features
-              such as making an appointment, connect to REST APIs, with Provider
-              as State Manager.
-            </li>
-            <li>
-              Developed user-friendly designs, layouts, and templates for mobile
-              applications.
-            </li>
-            <li>
-              Built reusable UI components with Flutter complementing material
-              design standards.
-            </li>
-          </ul>
-        </p>
-      </div>
+      <ol className="relative border-l border-gray-200 dark:border-gray-700">
+        <li className="mb-10 ml-4">
+          <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+          <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+            February 2024 - Present
+          </time>
+          <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+            Frontend Engineer - MySkill.id
+          </h3>
+        </li>
+        <li className="mb-10 ml-4">
+          <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+          <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+            October 2023 - February 2024
+          </time>
+          <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+            Frontend Engineer Trainee - Sea Labs Indonesia
+          </h3>
+        </li>
+        <li className="ml-4">
+          <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+          <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+            December 2022 - April 2023
+          </time>
+          <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+            Frontend Engineer - Diponegoro University
+          </h3>
+        </li>
+      </ol>
     </>
   );
 }
