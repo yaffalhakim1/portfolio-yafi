f8989c0e52a2789aa7477babeed50f8514d4196d
Muhammad Yafi Alhakim <yafialhakim64@gmail.com>
2024-09-14 10:14:44 +0700

    fix: pnpm problem and change work section

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,37 +1,6 @@
-import Writing from "@/components/contents-writing";
-import { getAllPublishedBlog } from "@/lib/notion";
-import Link from "next/link";
-
 export default async function BlogPage() {
-  // const posts = await fetchBlogData();
-
   return (
     <div>
-      {/* <h1 className="font-bold text-[36px]">Writings</h1>
-      <p>
-        Here are some of my writings. All of my writings are available on my{" "}
-        <span>
-          <a
-            href="https://yafialhakimblog.vercel.app/"
-            className="text-[#00DC82] hover:underline"
-          >
-            blog
-          </a>
-        </span>
-        .
-      </p>
-      <div className="flex flex-col gap-4 mt-8">
-        {posts.map((post) => (
-          <div key={post.id} className="flex flex-col  mt-4">
-            <Writing
-              title={post.title}
-              desc={post.description}
-              publishedDate={post.publishedDate}
-              slug={post.slug}
-            />
-          </div>
-        ))}
-      </div> */}
       <section className="">
         <div className="py-8 px-4 mx-auto max-w-screen-md text-center lg:py-16 lg:px-12">
           <svg
